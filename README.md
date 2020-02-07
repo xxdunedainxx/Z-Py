8bd06880afe5e631b706d5baecc110dbfeb3231d
xxdunedainxx <zrmmaster92@gmail.com>
2020-02-07 09:18:20 -0800

    local commits (start thread locking)

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Z-Py
 Python framework written by yours truly :) 
+
+### TODOS:
+**Setup.py**\
+**Integrating singleton into project**\
+**Config refactor**\
--- Workbench.py
+++ Workbench.py
@@ -4,6 +4,7 @@ override = "workbench"
 # General App Workbench testing
 from src.build.ApplicationCLI import DeveloperCLI
 
+
 def workbench():
     from src.workbench.main.app import ApplicationWorkbench
 
@@ -16,3 +17,35 @@ if override is None:
 else:
     print("blah")
     #method = globals()[override]()
+
+from threading import Thread, Lock
+
+
+from functools import wraps
+from time import sleep
+mutex = Lock()
+count = 0
+def lockable_resource(method):
+    @wraps(method)
+    def lock_logic(*args, **kwargs):
+        mutex.acquire()
+        try:
+            args.c = 1 + args.c
+
+            print("got it")
+            if args.c % 2 == 0:
+                sleep(3)
+            return method
+        finally:
+            mutex.release()
+    return  lock_logic()
+
+@lockable_resource
+def processData(c):
+    print('Do some stuff')
+
+while True:
+    t = Thread(target=processData, args=(count,))
+    t.start()
+    t2 = Thread(target=processData, args=(count,))
+    t2.start()
