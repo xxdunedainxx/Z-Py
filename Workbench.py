3bd4e7bbaf51ff340585c3b8693a4a5bff9cec5c
xxdunedainxx <zrmmaster92@gmail.com>
2020-02-07 00:22:42 -0800

    my head hurts

--- Workbench.py
+++ Workbench.py
@@ -41,26 +41,40 @@ class Reusable:
     pass
 
 
+def eval_kwargs(itemToCheck, evalItemTruth = False, **kwargs)->bool:
+    if kwargs is dict and "args" in kwargs.keys() and itemToCheck in kwargs.keys() and (evalItemTruth == False or kwargs["args"][itemToCheck] == True ):
+        return True
+    else:
+        return False
+
+
 # Singleton/ClassVariableSingleton.py
 class SingleTone(object):
     __instance = None
 
-    def __dispatch_object(self):
-        return object.__new__(self)
+    def __dispatch_object(cls):
+        return object.__new__(cls)
 
-    def __new__(self, **kwargs):
-        if "singleton" in kwargs["args"].keys() and kwargs["args"]["singleton"] == True:
-            # Singleton gate
+    def __new__(cls, **kwargs):
+
+        # Singleton gate
+        if "kwargs" in kwargs.keys() and "polymorphic_evolution" in kwargs['kwargs'].keys() and "singleton" in kwargs['kwargs']['polymorphic_evolution']:
             if SingleTone.__instance is None:
-                SingleTone.__instance = self.__dispatch_object(self)
+                SingleTone.__instance = object.__new__(cls)
             return SingleTone.__instance
         else:
-            return self.__dispatch_object(self)
+            return object.__new__(cls)
+
+    def ping_singleton(self):
+        return SingleTone.__instance is None
+
+    def get_singleton(self):
+        return SingleTone.__instance
 
 class TestPoly():
-    def __new__(self, **kwargs):
-        self.test_poly="test"
-        return object.__new__(self)
+    def __new__(cls, **kwargs):
+        cls.test_poly="test"
+        return object.__new__(cls)
 
 class PolymorphicGate(SingleTone,TestPoly):
     ObjectOverrides: dict = {
@@ -69,32 +83,53 @@ class PolymorphicGate(SingleTone,TestPoly):
     }
 
     def __polymorphic_evolution(self,**kwargs)->tuple:
-        t=tuple()
-        types=kwargs["polymorphic_evolution"] if "polymorphic_evolution" in kwargs.keys() else None
+        types=kwargs['kwargs']["polymorphic_evolution"] if "kwargs" in kwargs.keys() and "polymorphic_evolution" in kwargs['kwargs'].keys() else None
 
         if types is None:
-            return t
+            return object.__new__(self)
+        elif "singleton" not in types:
+            t = (object.__new__(self),)
+        elif self.ping_singleton(self):
+            t = tuple()
+        else:
+            return (self.get_singleton(self),)
+
 
         for type in types:
             if type in PolymorphicGate.ObjectOverrides.keys():
-                t = t + (PolymorphicGate.ObjectOverrides[type].__new__(self,kwargs),)
+                t = t + (PolymorphicGate.ObjectOverrides[type].__new__(PolymorphicGate.ObjectOverrides[type], **kwargs),)
         return t
 
-    def __new__(self, **kwargs):
-        polymorphicTuple: tuple = (object.__new__(self), self.__polymorphic_evolution(self,kwargs))
+    def __new__(cls, **kwargs):
+        additional_args= cls.__polymorphic_evolution(cls, kwargs=kwargs["args"])
+        polymorphicTuple: tuple = (additional_args,)
         return polymorphicTuple
 class Test(PolymorphicGate):
 
+    def __new__(cls, **kwargs):
+        super(Test,cls).__new__(cls, **kwargs)
+        print("blah")
+        return cls.__init__(cls,args=kwargs)
+
+
     def __init__(self, args:{}):
         print("singleton test")
+        super(Test,self).__init__(args)
+        print("blah")
+        return self
 
 
 
-t=Test(args={"polymorphic_evolution" : ["singleton"]})
+t=Test(args={"polymorphic_evolution" : ["singleton", "testpoly"]})
 print(t)
-t2=Test(args={"polymorphic_evolution" : ["singleton"]})
-t3=Test(args={"singleton" : False})
-print(t2)
-print(t3)
+t.test_poly="bla blah blah"
+t2=Test(args={"polymorphic_evolution" : ["singleton", "testpoly"]})
+t3=Test(args={"polymorphic_evolution" : [ "testpoly"]})
+print(hex(id(t)))
+print(hex(id(t2)))
+print(hex(id(t3)))
+t3.test_poly = "hey there"
 print(isinstance(t3,SingleTone))
+print(isinstance(t,SingleTone))
+print(isinstance(t2,SingleTone))
 exit(0)
